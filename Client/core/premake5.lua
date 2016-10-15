1efad428f330fdf84925b5530188eea348718c56
Jusonex <jusonex96@gmail.com>
2016-10-16 01:24:31 +0200

    Remove duplicate manifest setting

--- Client/core/premake5.lua
+++ Client/core/premake5.lua
@@ -6,7 +6,7 @@ project "Client Core"
 	
 	filter "system:windows"
 		includedirs { "../../vendor/sparsehash/current/src/windows" }
-		linkoptions { "/SAFESEH\:NO", "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"" }
+		linkoptions { "/SAFESEH\:NO" }
 		buildoptions { "-Zm130" }
 		files { "compatibility.manifest" }
 	
